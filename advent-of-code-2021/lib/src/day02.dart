be612e71be733f412f1fb438ab608d0cbdb908a3
Miguel Scudeller <miguelscudeller@gmail.com>
2021-12-02 02:56:24 -0300

    Add day02

--- advent-of-code-2021/lib/src/day02.dart
+++ advent-of-code-2021/lib/src/day02.dart
@@ -1,17 +1,109 @@
 import '../day.dart';
 
 class Day02 implements Day {
-  final List<String> _input; // ignore: unused_field
+  final List<Command> _input;
 
-  Day02(this._input);
+  Day02(List<String> input) : _input = input.map((e) => Command(e)).toList();
 
   @override
   part1() {
-    return null;
+    var position = _input.fold<Position>(Position(), (p, c) => c.run1(p));
+    return position.horizontalPosition * position.depth;
   }
 
   @override
   part2() {
-    return null;
+    var position = _input.fold<Position>(Position(), (p, c) => c.run2(p));
+    return position.horizontalPosition * position.depth;
+  }
+}
+
+class Position {
+  int horizontalPosition;
+  int depth;
+  int aim;
+
+  Position()
+      : horizontalPosition = 0,
+        depth = 0,
+        aim = 0;
+}
+
+abstract class Command {
+  abstract int quantity;
+
+  factory Command(String command) {
+    var foo = command.split(' ');
+    var quantity = int.parse(foo[1]);
+    switch (foo[0]) {
+      case 'forward':
+        return Forward(quantity);
+      case 'down':
+        return Down(quantity);
+      case 'up':
+        return Up(quantity);
+    }
+    throw ArgumentError();
+  }
+
+  Position run1(Position position);
+  Position run2(Position position);
+}
+
+class Forward implements Command {
+  @override
+  int quantity;
+
+  Forward(this.quantity);
+
+  @override
+  Position run1(Position position) {
+    position.horizontalPosition += quantity;
+    return position;
+  }
+
+  @override
+  Position run2(Position position) {
+    position.horizontalPosition += quantity;
+    position.depth += position.aim * quantity;
+    return position;
+  }
+}
+
+class Down implements Command {
+  @override
+  int quantity;
+
+  Down(this.quantity);
+
+  @override
+  Position run1(Position position) {
+    position.depth += quantity;
+    return position;
+  }
+
+  @override
+  Position run2(Position position) {
+    position.aim += quantity;
+    return position;
+  }
+}
+
+class Up implements Command {
+  @override
+  int quantity;
+
+  Up(this.quantity);
+
+  @override
+  Position run1(Position position) {
+    position.depth -= quantity;
+    return position;
+  }
+
+  @override
+  Position run2(Position position) {
+    position.aim -= quantity;
+    return position;
   }
 }
---
+++ advent-of-code-2021/test/day02_test.dart
@@ -0,0 +1,20 @@
+import 'package:test/test.dart';
+import 'package:advent_of_code_2021/day.dart';
+
+void main() {
+  late Day day;
+  setUp(() {
+    return day = Day.input(
+        2, ['forward 5', 'down 5', 'forward 8', 'up 3', 'down 8', 'forward 2']);
+  });
+  group('Day02', () {
+    group('.part1', () {
+      test('.part1 1st example', () {
+        expect(day.part1(), 150);
+      });
+      test('.part2 1st example', () {
+        expect(day.part2(), 900);
+      });
+    });
+  });
+}
