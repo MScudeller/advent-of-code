a1d2e77e2c306028d3760a8d75b1d2bc3a54bd2d
Miguel Scudeller <miguelscudeller@gmail.com>
2021-12-03 03:46:08 -0300

    Code improvements

--- advent-of-code-2021/lib/src/day03.dart
+++ advent-of-code-2021/lib/src/day03.dart
@@ -1,64 +1,70 @@
 import '../day.dart';
 
 class Day03 implements Day {
-  final List<String> _input; // ignore: unused_field
+  final List<String> _input;
+  final int binarySize;
 
-  Day03(this._input);
+  Day03(this._input) : binarySize = _input[0].length;
 
   @override
   part1() {
     var gammaString = '';
-    for(var i = 0; i < _input[0].length; i++) {
-      int foo = (_input.length~/2);
-      var bar = _input.map((e) => e[i]).toList()..sort();
-      gammaString += bar[foo];
-    }
-
     var epsilonString = '';
-    for(var i = 0; i < gammaString.length; i++) {
-      epsilonString += gammaString[i] == '0' ? '1' : '0';
-    }
-    var gamma = int.parse(gammaString, radix:2);
-    var epsilon = int.parse(epsilonString, radix:2);
 
-    return gamma*epsilon;
+    for (var i = 0; i < binarySize; i++) {
+      var mc = mostCommon(getPositionList(_input, i));
+      gammaString += mc;
+      epsilonString += invertBinary(mc);
+    }
+
+    var gamma = int.parse(gammaString, radix: 2);
+    var epsilon = int.parse(epsilonString, radix: 2);
+
+    return gamma * epsilon;
+  }
+
+  List<String> getPositionList(List<String> list, int i) {
+    return list.map((s) => s[i]).toList();
+  }
+
+  String mostCommon(List<String> list) {
+    int foo = (list.length ~/ 2);
+    var bar = list..sort();
+    return bar[foo];
+  }
+
+  String invertBinary(String binary) {
+    return binary == '0' ? '1' : '0';
   }
 
   @override
   part2() {
     var options = _input.toList();
-    for(var i = 0; i < _input[0].length; i++) {
-      var mc = mostCommon(options.map((s) => s[i]).toList());
+    for (var i = 0; i < binarySize; i++) {
+      var mc = mostCommon(getPositionList(options, i));
       options = options.where((e) => e[i] == mc).toList();
-      if(options.length == 1){
+      if (options.length == 1) {
         break;
       }
     }
     var ogrString = options.first;
 
     options = _input.toList();
-    for(var i = 0; i < _input[0].length; i++) {
-      var mc = leastCommon(options.map((s) => s[i]).toList());
+    for (var i = 0; i < binarySize; i++) {
+      var mc = leastCommon(getPositionList(options, i));
       options = options.where((e) => e[i] == mc).toList();
-      if(options.length == 1){
+      if (options.length == 1) {
         break;
       }
     }
     var csrString = options.first;
-    var ogr = int.parse(ogrString, radix:2);
-    var csr = int.parse(csrString, radix:2);
+    var ogr = int.parse(ogrString, radix: 2);
+    var csr = int.parse(csrString, radix: 2);
 
-    return ogr*csr;
+    return ogr * csr;
   }
 
-  String mostCommon(List<String> list){
-    int foo = (list.length~/2);
-    var bar = list..sort();
-    return bar[foo];
-  }
-
-  String leastCommon(List<String> list){
-    var mc = mostCommon(list);
-    return mc == '0' ? '1' : '0';
+  String leastCommon(List<String> list) {
+    return invertBinary(mostCommon(list));
   }
 }
