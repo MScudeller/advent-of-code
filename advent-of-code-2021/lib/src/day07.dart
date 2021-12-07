348d3c4933fe2bcb9763a80a763b69762d2eddba
Miguel Scudeller <miguelscudeller@gmail.com>
2021-12-07 02:44:31 -0300

    improve day 7

--- advent-of-code-2021/lib/src/day07.dart
+++ advent-of-code-2021/lib/src/day07.dart
@@ -4,34 +4,38 @@ import '../day.dart';
 
 class Day07 implements Day {
   final List<int> _input;
+  final int biggest;
 
-  Day07(List<String> input)
-      : _input = input.first.split(',').map(int.parse).toList();
+  factory Day07(List<String> input) {
+    var list = input.first.split(',').map(int.parse).toList();
+    var big = list.reduce((value, element) => max(value, element));
+    return Day07.n(list, big);
+  }
+
+  Day07.n(this._input, this.biggest);
 
   @override
   part1() {
-    var biggest = _input.fold<int>(
-        0, (previousValue, element) => max(previousValue, element));
-    var a = List.generate(biggest, (i) => i);
-    return a
+    return List.generate(biggest, (i) => i)
         .map((i) => _input
-            .map((e) => (e - i).abs())
+            .map((e) => abs(e, i))
             .reduce((value, element) => value + element))
         .reduce((value, element) => min(value, element));
   }
 
+  int abs(int e, int i) => (e - i).abs();
+
   @override
   part2() {
-    var biggest = _input.fold<int>(
-        0, (previousValue, element) => max(previousValue, element));
-    var a = List.generate(biggest, (i) => i);
-    return a
+    return List.generate(biggest, (i) => i)
         .map((i) => _input
-            .map((e) {
-              var dist = (e - i).abs();
-              return (dist + 1) * dist / 2;
-            })
+            .map((e) => summation(e, i))
             .reduce((value, element) => value + element))
         .reduce((value, element) => min(value, element));
+  }
+
+  double summation(int e, int i) {
+    var dist = abs(e, i);
+    return (dist + 1) * dist / 2;
   }
 }
