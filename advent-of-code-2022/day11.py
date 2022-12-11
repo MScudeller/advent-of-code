d6dd437deda383fb8ce8e94f20f86c24aa4bfb32
Miguel Scudeller <miguelscudeller@gmail.com>
2022-12-11 04:33:47 -0300

    improve code organization

--- advent-of-code-2022/day11.py
+++ advent-of-code-2022/day11.py
@@ -2,55 +2,54 @@ import math
 
 from aoc import get_input
 
-lines = get_input(11).splitlines()
+
+def get_example():
+    return [
+        "Monkey 0:",
+        "  Starting items: 79, 98",
+        "  Operation: new = old * 19",
+        "  Test: divisible by 23",
+        "    If true: throw to monkey 2",
+        "    If false: throw to monkey 3",
+        "",
+        "Monkey 1:",
+        "  Starting items: 54, 65, 75, 74",
+        "  Operation: new = old + 6",
+        "  Test: divisible by 19",
+        "    If true: throw to monkey 2",
+        "    If false: throw to monkey 0",
+        "",
+        "Monkey 2:",
+        "  Starting items: 79, 60, 97",
+        "  Operation: new = old * old",
+        "  Test: divisible by 13",
+        "    If true: throw to monkey 1",
+        "    If false: throw to monkey 3",
+        "",
+        "Monkey 3:",
+        "  Starting items: 74",
+        "  Operation: new = old + 3",
+        "  Test: divisible by 17",
+        "    If true: throw to monkey 0",
+        "    If false: throw to monkey 1",
+    ]
 
 
-# lines = [
-# "Monkey 0:",
-# "  Starting items: 79, 98",
-# "  Operation: new = old * 19",
-# "  Test: divisible by 23",
-# "    If true: throw to monkey 2",
-# "    If false: throw to monkey 3",
-# "",
-# "Monkey 1:",
-# "  Starting items: 54, 65, 75, 74",
-# "  Operation: new = old + 6",
-# "  Test: divisible by 19",
-# "    If true: throw to monkey 2",
-# "    If false: throw to monkey 0",
-# "",
-# "Monkey 2:",
-# "  Starting items: 79, 60, 97",
-# "  Operation: new = old * old",
-# "  Test: divisible by 13",
-# "    If true: throw to monkey 1",
-# "    If false: throw to monkey 3",
-# "",
-# "Monkey 3:",
-# "  Starting items: 74",
-# "  Operation: new = old + 3",
-# "  Test: divisible by 17",
-# "    If true: throw to monkey 0",
-# "    If false: throw to monkey 1",
-# ]
-
-
-def parse_monkeys(input):
+def parse_monkeys(lines):
     monkeys = []
-    for i in range(int((len(input) + 1) / 7)):
+    for i in range(int((len(lines) + 1) / 7)):
         starting_line = i * 7
-        starting_items = [int(item) for item in input[starting_line + 1][18:].split(", ")]
-        operation = (lambda x, y: x * y) if input[starting_line + 2][23:24] == "*" else (lambda x, y: x + y)
-        operation_amount = input[starting_line + 2][25:]
-        test = int(input[starting_line + 3][21:])
-        test_true = int(input[starting_line + 4][29:])
-        test_false = int(input[starting_line + 5][30:])
+        starting_items = [int(item) for item in lines[starting_line + 1][18:].split(", ")]
+        operation = (lambda x, y: x * y) if lines[starting_line + 2][23:24] == "*" else (lambda x, y: x + y)
+        operation_amount = lines[starting_line + 2][25:]
+        test = int(lines[starting_line + 3][21:])
+        test_true = int(lines[starting_line + 4][29:])
+        test_false = int(lines[starting_line + 5][30:])
         monkeys.append([starting_items, operation, operation_amount, test, test_true, test_false, 0])
     return monkeys
 
 
-def run(monkeys, stop, worry_modifier):
+def simulate(monkeys, stop, worry_modifier):
     for j in range(stop):
         for m in monkeys:
             for item in m[0]:
@@ -66,12 +65,17 @@ def run(monkeys, stop, worry_modifier):
             m[0].clear()
     active = [m[6] for m in monkeys]
     active.sort(reverse=True)
-    print(active[0] * active[1])
+    return active[0] * active[1]
 
 
-monkeys = parse_monkeys(lines)
-run(monkeys, 20, lambda x: x//3)
+def run():
+    lines = get_input(11).splitlines()
+    # lines = get_example()
+    monkeys = parse_monkeys(lines)
+    print(simulate(monkeys, 20, lambda x: x // 3))
+    monkeys = parse_monkeys(lines)
+    mmc = math.prod(m[3] for m in monkeys)
+    print(simulate(monkeys, 10000, lambda x: x % mmc))
 
-monkeys = parse_monkeys(lines)
-mmc = math.prod(m[3] for m in monkeys)
-run(monkeys, 10000, lambda x: x % mmc)
+
+run()
